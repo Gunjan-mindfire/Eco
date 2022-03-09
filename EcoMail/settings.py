a1b312579ab7deb097d5f89f83fab1406a448c81
Gunjan-mindfire <mfsi.gunjan@gmail.com>
2022-03-09 06:58:48 +0000

    allowed host

--- EcoMail/settings.py
+++ EcoMail/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.202.51.93']
 
 
 # Application definition
